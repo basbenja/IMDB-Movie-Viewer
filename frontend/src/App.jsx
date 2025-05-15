340a421e48a33b501f9672b3f9fcb763b19154ee
Benjamín Bas <bbas@ascentio.com.ar>
2025-05-15 12:51:04 -0300

    feat: changed app background

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,9 @@ import FilterableMovieCatalog from './components/FilterableMovieCatalog';
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 px-4">
+    <div className="min-h-screen flex flex-col items-center justify-center
+    bg-[radial-gradient(ellipse_at_top,theme(colors.violet.950),theme(colors.gray.900),black)] px-4"
+  >
       <h1 className="glow-text text-7xl font-semibold mb-10 text-center text-white">Movie Viewer 🍿</h1>
       <FilterableMovieCatalog />
     </div>
