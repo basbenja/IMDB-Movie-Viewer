4af82f25e1a6f481ccf35af4db268fac7be9a82c
Benjamín Bas <bbas@ascentio.com.ar>
2025-05-15 12:13:50 -0300

    feat: style to search bar

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -6,7 +6,9 @@ function SearchBar ({ searchTerm, onSearchTermChange }) {
         value={searchTerm}
         onChange={(e) => onSearchTermChange(e.target.value)}
         placeholder="Search for a movie by its title..."
-        className="w-full text-white px-4 py-2 border border-gray-300 rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
+        className="w-full text-white px-4 py-2 border border-gray-300 rounded-xl
+        shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400 hover:ring-2 hover:ring-blue-400
+        transition duration-200 ease-in-out bg-gray-800 placeholder-gray-500"
       />
     </form>
   );
