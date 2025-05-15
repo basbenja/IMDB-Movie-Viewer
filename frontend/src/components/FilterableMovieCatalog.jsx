7e555bd856fad74ac672883619c64705f00089d9
Benjamín Bas <bbas@ascentio.com.ar>
2025-05-15 12:52:54 -0300

    feat: show upload button when there are movies

--- frontend/src/components/FilterableMovieCatalog.jsx
+++ frontend/src/components/FilterableMovieCatalog.jsx
@@ -11,12 +11,27 @@ function FilterableMovieCatalog() {
   const [movies, setMovies] = useState([]);
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
-  const moviesPerPage = 5;
+  const [moviesPerPage, setMoviesPerPage] = useState(5);
 
   function handleSearchTermChange(term) {
     setSearchTerm(term);
     setPage(1);
   };
+
+  useEffect(() => {
+    function updateMoviesPerPage() {
+      const width = window.innerWidth;
+
+      if (width < 640) setMoviesPerPage(1); // sm
+      else if (width < 768) setMoviesPerPage(2); // md
+      else if (width < 1024) setMoviesPerPage(3); // lg
+      else if (width < 1280) setMoviesPerPage(4); // xl
+      else setMoviesPerPage(5); // 2xl and above
+    }
+
+    updateMoviesPerPage();
+    window.addEventListener("resize", updateMoviesPerPage);
+  }, []);
 
   useEffect(() => {
     if (!uploadSuccess && movies.length === 0) return;
@@ -36,7 +51,7 @@ function FilterableMovieCatalog() {
     }
 
     fetchMovies();
-  }, [page, searchTerm, uploadSuccess]); // re-run when page or uploadSuccess changes
+  }, [page, searchTerm, uploadSuccess, moviesPerPage]); // re-run when page or uploadSuccess changes
 
   if (movies.length === 0 && !uploadSuccess) {
     return (
@@ -47,23 +62,25 @@ function FilterableMovieCatalog() {
         <FileUploader onUploadSucess={setUploadSucess} />
       </div>
     )
-  } else if (movies.length === 0 && uploadSuccess) {
+  } else if (uploadSuccess) {
     return (
       <div className="flex flex-col items-center justify-center">
-        <SearchBar searchTerm={searchTerm} onSearchTermChange={setSearchTerm}/>
+        <div className="flex flex-row items-center justify-center gap-4 w-full max-w-4xl mb-6">
+          <SearchBar searchTerm={searchTerm} onSearchTermChange={handleSearchTermChange} />
+          <FileUploader onUploadSucess={setUploadSucess} />
+        </div>
+
+      {(movies.length === 0) ? (
         <h2 className="text-xl font-medium mb-4 text-gray-500">
             No movies found for the search term "{searchTerm}". Please try again.
         </h2>
-      </div>
-    )
-  } else {
-    return (
-      <div className="flex flex-col items-center justify-center">
-          <SearchBar searchTerm={searchTerm} onSearchTermChange={handleSearchTermChange} />
-          <MovieCatalog movies={movies} onPageChange={setPage} currentPage={page} totalPages={totalPages} />
-          <h4 className="text-lg font-medium mt-6 text-gray-500">
-            Page <NumberFlow value={page} /> of {totalPages}
-          </h4>
+      ) : (
+        <MovieCatalog movies={movies} onPageChange={setPage} currentPage={page} totalPages={totalPages} />
+      )}
+        <h4 className="text-lg font-medium mt-6 text-gray-500">
+          Page <NumberFlow value={page} /> of {totalPages}
+        </h4>
+
       </div>
     )
   }
