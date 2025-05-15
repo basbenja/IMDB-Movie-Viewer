3ff193e10fdb2666e75285e8776e2cc1559e61a2
Benjamín Bas <bbas@ascentio.com.ar>
2025-05-15 14:01:38 -0300

    feat: added file uploading error handling

--- frontend/src/components/FileUploader.jsx
+++ frontend/src/components/FileUploader.jsx
@@ -4,6 +4,7 @@ import { useRef, useState } from "react";
 
 function FileUploader({ onUploadSucess }) {
   const [loading, setLoading] = useState(false);
+  const [uploadError, setUploadError] = useState(null);
 
   const inputRef = useRef();
 
@@ -25,13 +26,20 @@ function FileUploader({ onUploadSucess }) {
       if (response.status === 200) {
         const data = response.data;
         onUploadSucess(data.movies ? true : false);
-        console.log("File uploaded successfully:", data);
-      } else {
-        console.error("Error uploading file:", response.statusText);
-        throw new Error("Error uploading file");
+        setUploadError(null);
       }
     } catch (error) {
-        console.error("Error uploading file:", error);
+      if (error.response) {
+        if (error.response.status === 415) {
+          setUploadError("Unsupported file type. Please upload a CSV file.");
+        } else {
+          setUploadError("Error uploading file. Please try again.");
+        }
+      } else {
+        // Network error or other issues
+        setUploadError("Error uploading file. Please try again.");
+      }
+      console.error("Error uploading file:", error);
     } finally {
       setLoading(false);
     }
@@ -39,34 +47,40 @@ function FileUploader({ onUploadSucess }) {
 
 
   return (
-    <div className="flex flex-row items-center mb-4">
-      <button
-        className="cursor-pointer px-6 py-2 bg-violet-600 text-white font-semibold
-        rounded hover:bg-violet-400 transition mr-2"
-        onClick={handleClick}
-      >
-        {loading ? "Uploading..." : "Upload Movie File"}
-      </button>
+    <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-row items-center mb-4">
+        <button
+          className="cursor-pointer px-6 py-2 bg-violet-600 text-white font-semibold
+          rounded hover:bg-violet-400 transition mr-2"
+          onClick={handleClick}
+        >
+          {loading ? "Uploading..." : "Upload Movie File"}
+        </button>
 
-      <div className="relative group mr-2">
-        <InformationCircleIcon className="h-6 w-6 text-white cursor-pointer" />
-        <div className="absolute left-full top-1/2 -translate-y-1/2 ml-2 w-64 bg-white text-gray-800 text-sm px-4 py-2 rounded shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-200 z-10">
-          For now, we only accept CSV files that must have the columns <br />
-          <strong>title, year, genre, description</strong>
+        <div className="relative group">
+          <InformationCircleIcon className="h-6 w-6 text-white cursor-pointer" />
+          <div className="absolute left-full top-1/2 -translate-y-1/2 ml-2 w-64 bg-white text-gray-800 text-sm px-4 py-2 rounded shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-200 z-10">
+            For now, we only accept CSV files that must have the columns <br />
+            <strong>title, year, genre, description</strong>
+          </div>
         </div>
+
+        {loading && (
+          <div className="animate-spin rounded-full h-6 w-6 border-4 border-t-transparent border-white"></div>
+        )}
+
+        <input
+          type="file"
+          accept=".csv"
+          ref={inputRef}
+          className="hidden"
+          onChange={(e) => handleFileUpload(e)}
+        />
       </div>
 
-      {loading && (
-        <div className="animate-spin rounded-full h-6 w-6 border-4 border-t-transparent border-white"></div>
+      {uploadError && (
+        <div className="text-sm text-red-500">{uploadError}</div>
       )}
-
-      <input
-        type="file"
-        accept=".csv"
-        ref={inputRef}
-        className="hidden"
-        onChange={(e) => handleFileUpload(e)}
-      />
     </div>
   );
 }
